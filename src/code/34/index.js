6c78387c6c220690c1a44cd75e1843b92189cabb
zoushijun <zoushijun@threatbook.cn>
2020-06-25 07:39:54 +0800

    add 34

---
+++ src/code/34/index.js
@@ -0,0 +1,24 @@
+export default (nums, target) => {
+  let end = nums.length - 1;
+  let begin = 0;
+  let mid = (begin + end) / 2;
+
+  while (begin <= end) {
+    if (nums[mid] < target) {
+      begin = mid + 1;
+      mid = (begin + end) / 2;
+    }
+    if (nums[mid] > target) {
+      end = mid - 1;
+      mid = (begin + end) / 2;
+    }
+    if (nums[mid] === target) {
+      for (; begin < mid; begin++) {
+        if (nums[begin] === target) {
+          return [begin, mid];
+        }
+      }
+    }
+  }
+  return [-1, -1];
+};
