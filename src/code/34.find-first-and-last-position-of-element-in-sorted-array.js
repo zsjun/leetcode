532bfc6ae7f1cf340d91226ceb0d0b6cfe43b09b
zsj <zoushijun@threatbook.cn>
2020-06-29 15:52:22 +0800

    fix

---
+++ src/code/34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=34 lang=javascript
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {};
+// @lc code=end
