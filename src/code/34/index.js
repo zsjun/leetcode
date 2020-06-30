c0ebf2b48d650c2989c19f9d74878131be843184
zoushijun <zoushijun@threatbook.cn>
2020-07-01 07:14:49 +0800

    change code/34/index.js

--- src/code/34/index.js
+++ src/code/34/index.js
@@ -1,16 +1,16 @@
 export default (nums, target) => {
   let end = nums.length - 1;
   let begin = 0;
-  let mid = (begin + end) / 2;
+  let mid = Math.floor((begin + end) / 2);
 
   while (begin <= end) {
     if (nums[mid] < target) {
       begin = mid + 1;
-      mid = (begin + end) / 2;
+      mid = Math.floor((begin + end) / 2);
     }
     if (nums[mid] > target) {
       end = mid - 1;
-      mid = (begin + end) / 2;
+      mid = Math.floor((begin + end) / 2);
     }
     if (nums[mid] === target) {
       for (; begin < mid; begin++) {
