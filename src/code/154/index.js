1da8eeb7f03c4952733e9b1094b6d3b1cc19c7d8
zoushijun <zoushijun@threatbook.cn>
2020-07-01 08:32:35 +0800

    add 154 leetcode

---
+++ src/code/154/index.js
@@ -0,0 +1,29 @@
+//
+export default (nums = []) => {
+  if (nums.length === 1) return nums[0];
+  let begin = 0;
+  let end = nums.length - 1;
+  let min = nums[begin];
+  while (begin <= end) {
+    if (nums[begin] > nums[end]) {
+      begin++;
+      if (nums[end] < min) {
+        min = nums[end];
+      }
+    }
+    if (nums[begin] < nums[end]) {
+      end--;
+      if (nums[begin] < min) {
+        min = nums[begin];
+      }
+    }
+    if (nums[begin] === nums[end]) {
+      begin++;
+      end--;
+      if (nums[begin] < min) {
+        min = nums[begin];
+      }
+    }
+  }
+  return min;
+};
---
+++ src/test/154/index.test.js
@@ -0,0 +1,24 @@
+import findMin from "../../code/154/index";
+
+// test(`arr:1`, () => {
+//   expect(getMaxRect([])).toBe(0);
+// });
+
+// test(`arr:1`, () => {
+//   expect(getTargetPosition([5, 7, 7, 8, 8, 10, 11], 8)).toEqual([3, 4]);
+// });
+
+// [1, 2];
+
+test(`arr:2`, () => {
+  expect(findMin([1, 3, 5])).toBe(1);
+});
+test(`arr:3`, () => {
+  expect(findMin([2, 2, 2, 0, 1])).toEqual(0);
+});
+test(`arr:5`, () => {
+  expect(findMin([1, 1])).toEqual(1);
+});
+test(`arr:6`, () => {
+  expect(findMin([3, 1, 3])).toEqual(1);
+});
