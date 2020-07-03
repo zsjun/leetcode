8cce4f8b0e4e0bedf254dd361f5a76eb2e7c1a58
zsj <zoushijun@threatbook.cn>
2020-07-03 19:04:40 +0800

    add

---
+++ createDir.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+const mkdirp = require("mkdirp");
+
+const nameArr = process.argv.slice(2);
+
+const fileName = nameArr.join("_");
+
+mkdirp(`./test/${fileName}`).then(() => console.log(`made test dir ${fileName}`));
+mkdirp(`./src/${fileName}`).then(() => console.log(`made src dir  ${fileName}`));
