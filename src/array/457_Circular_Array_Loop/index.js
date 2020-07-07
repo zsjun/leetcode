f01f9081858aec6d621d6413af633edba1082659
zsj <zoushijun@threatbook.cn>
2020-07-07 18:25:11 +0800

    finish leetcode 457

--- src/array/457_Circular_Array_Loop/index.js
+++ src/array/457_Circular_Array_Loop/index.js
@@ -2,18 +2,13 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+ int n = nums.length;
+ return i + nums[i] >= 0? (i + nums[i]) % n: n + ((i + nums[i]) % n);
 // 获取nums 中i的下一步在nums中的位置
 const getNextPosition = (nums, i) => {
   let _num = i + nums[i];
   const len = nums.length;
-  if (_num > 0 && _num < len) {
-    return _num;
-  } else if (_num > 0 && _num >= len) {
-    _num = _num % len;
-  } else if (_num <= 0) {
-    _num = (len + _num) % len;
-  }
-  return _num;
+  return _num>= 0? (i + nums[i]) % n: n + ((i + nums[i]) % n);;
 };
 function circularArrayLoop(nums) {
   if (!nums || nums.length === 1) return false;
@@ -24,28 +19,24 @@ function circularArrayLoop(nums) {
     }
     let j = i;
     let k = getNextPosition(nums, i);
-    let nextK = getNextPosition(nums, k);
-    while (nums[j] * nums[k] > 0 && nums[k] * nums[nextK] > 0) {
-      console.log(j);
-      console.log(nextK);
-      if (j === nextK) {
-        // 只有一个时候，形成死循环
-        if (j === getNextPosition(nums, j)) {
+    while (nums[k] * nums[i] > 0 && nums[getNextPosition(nums, k)] * nums[i] > 0) {
+      if (j == k) {
+        // check for loop with only one element
+        if (j == getNextPosition(nums, j)) {
           break;
         }
         return true;
       }
-      j = k;
-      k = nextK;
-      nextK = getNextPosition(nums, nextK);
+      j = getNextPosition(nums, j);
+      k = getNextPosition(nums, getNextPosition(nums, k));
     }
     // 如果没有找到
     j = i;
-    let val = nums[j];
-    let nextJ = getNextPosition(nums, j);
-    while (nums[nextJ] * val < 0) {
-      nums[nextJ] = 0;
-      nextJ = getNextPosition(nums, nextJ);
+    let val = nums[i];
+    while (nums[j] * val > 0) {
+      let next = getNextPosition(nums, j);
+      nums[j] = 0;
+      j = next;
     }
   }
   return false;
--- test/array/457_Circular_Array_Loop/index.test.js
+++ test/array/457_Circular_Array_Loop/index.test.js
@@ -3,12 +3,20 @@ import circularArrayLoop from "../../../src/array/457_Circular_Array_Loop/index"
 test("1", () => {
   expect(circularArrayLoop([2, -1, 1, 2, 2])).toBe(true);
 });
-test("1", () => {
+test("2", () => {
   expect(circularArrayLoop([-1, 2])).toBe(false);
 });
-test("1", () => {
+test("3", () => {
   expect(circularArrayLoop([-2, 1, -1, -2, -2])).toBe(false);
 });
-// test("1", () => {
-//   expect(circularArrayLoop([2, -1, 1, 2, 2])).toBe(true);
-// });
+test("4", () => {
+  expect(circularArrayLoop([-1, 2, 1, 2])).toBe(true);
+});
+
+test("5", () => {
+  expect(circularArrayLoop([1, 1, 1, 1, 1, 1, 1, 1, 1, -5])).toBe(false);
+});
+
+test("6", () => {
+  expect(circularArrayLoop([-2, -17, -1, -2, -2])).toBe(true);
+});
