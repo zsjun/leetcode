b4760421f5eddc5ac27c1b45c2d832cc0740f114
zsj <zoushijun@threatbook.cn>
2020-07-22 12:18:04 +0800

    change backtrack/78 and 90

--- src/backtrack/78_Subsets/index1.js
+++ src/backtrack/78_Subsets/index1.js
@@ -2,12 +2,9 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-//[1,2,2]
 const backtrack = (list, tempList, nums, i) => {
-  // let _temp = [...tempList];
   list.push([...tempList]);
   for (let k = i; k < nums.length; k++) {
-    if (k > i && nums[k - 1] === nums[k]) continue;
     tempList.push(nums[k]);
     backtrack(list, tempList, nums, k + 1);
     tempList.splice(tempList.length - 1, 1);
@@ -19,6 +16,5 @@ export default function subsets(nums) {
   let tempList = [];
   nums.sort((a, b) => a - b);
   backtrack(list, tempList, nums, 0);
-  console.log(list);
   return list;
 }
--- test/backtrack/78_Subsets/index.test.js
+++ test/backtrack/78_Subsets/index.test.js
@@ -1,4 +1,4 @@
-import subsets from "../../../src/backtrack/78_Subsets/index";
+import subsets from "../../../src/backtrack/78_Subsets/index1";
 
 test("1", () => {
   expect(subsets([1, 2, 3])).toEqual([[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]);
