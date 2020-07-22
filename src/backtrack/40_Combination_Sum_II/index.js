ee8495384655abdfb9c98b81812e18f5f2bc7faa
zsj <zoushijun@threatbook.cn>
2020-07-22 12:05:29 +0800

    backtrack/40

--- src/backtrack/40_Combination_Sum_II/index.js
+++ src/backtrack/40_Combination_Sum_II/index.js
@@ -4,13 +4,15 @@
  */
 const backtrack = (list, tempList, nums, i, target) => {
   let _temp = [...tempList];
-  const count = _temp.reduce((a, b) => a + b, 0);
-  if (count === target) {
+  if (target < 0 || i > nums.length) return;
+  if (target === 0) {
     list.push([...tempList]);
   }
+
   for (let k = i; k < nums.length; k++) {
+    if (k > i && nums[k] === nums[k - 1]) continue;
     _temp.push(nums[k]);
-    backtrack(list, _temp, nums, k + 1, target);
+    backtrack(list, _temp, nums, k + 1, target - nums[k]);
     _temp = _temp.slice(0, _temp.length - 1);
   }
 };
--- test/backtrack/40_Combination_Sum_II/index.test.js
+++ test/backtrack/40_Combination_Sum_II/index.test.js
@@ -1,10 +1,13 @@
 import subsets from "../../../src/backtrack/40_Combination_Sum_II/index";
 
+// test("1", () => {
+//   expect(subsets([10, 1, 2, 7, 6, 1, 5], 8)).toEqual([
+//     [1, 1, 6],
+//     [1, 2, 5],
+//     [1, 7],
+//     [2, 6],
+//   ]);
+// });
 test("1", () => {
-  expect(subsets([10, 1, 2, 7, 6, 1, 5], 8)).toEqual([
-    [1, 7],
-    [1, 2, 5],
-    [1, 1, 6],
-    [2, 6],
-  ]);
+  expect(subsets([2, 5, 2, 1, 2], 5)).toEqual([[1, 2, 2], [5]]);
 });
