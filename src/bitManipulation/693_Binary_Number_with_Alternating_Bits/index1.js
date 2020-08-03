ed9c72915d6e919c56492591e05f6f7fadee6fc7
zsj <zoushijun@threatbook.cn>
2020-08-03 21:17:01 +0800

    add

--- src/bitManipulation/693_Binary_Number_with_Alternating_Bits/index1.js
+++ src/bitManipulation/693_Binary_Number_with_Alternating_Bits/index1.js
@@ -1,12 +1,10 @@
 export default (n) => {
   let pre = n & 1;
   n >>= 1;
-  console.log(pre, n);
   while (n > 0) {
     if (((n & 1) ^ pre) === 0) return false;
     pre = n & 1;
     n >>= 1;
-    console.log(pre, n);
   }
   return true;
 };
