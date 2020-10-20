219520a29d7d8b3cc4268383fd5d9d19d7488a93
zsj <zoushijun@threatbook.cn>
2020-10-20 16:37:21 +0800

    add top_interview/10

--- src/top_interview/10_Regular_Expression_Matching/index.js
+++ src/top_interview/10_Regular_Expression_Matching/index.js
@@ -7,55 +7,42 @@
 const isMatch = (s, p) => {
   const sLen = s.length;
   const pLen = p.length;
+
+  if (s == null || p == null) {
+    return false;
+  }
+
   let dp = [];
-  for (let i = 0; i < sLen; i++) {
-    dp[i] = new Array(pLen).fill(false);
+  for (let i = 0; i <= sLen; i++) {
+    dp[i] = new Array(pLen + 1).fill(false);
   }
   dp[0][0] = true;
-  // dp[0][0] = false;
-  // dp[1][0] = false;
-  // dp[0][1] = false;
-
-  // if (s[0] === p[0] || p[0] === ".") {
-  //   dp[0][0] = true;
-  // } else {
-  //   dp[0][0] = false;
-  // }
-  // if (dp[0][0] && p[1] === "*") {
-  //   dp[0][1] = true;
-  // } else {
-  //   dp[0][1] = false;
-  // }
-  // dp[1][0] = false;
-  dp[0][1] = false;
-  for (let j = 1; j < p.length; j++) {
-    if (p[j] === "*" && dp[0][j - 1]) {
-      dp[0][j] = true;
-    } else {
-      dp[0][j] = false;
+  for (let j = 0; j < pLen; j++) {
+    if (p[j] === "*" && j >= 1 && dp[0][j - 1]) {
+      dp[0][j + 1] = true;
     }
   }
-  // for (let j = 1; j < s.length; j++) {
-  //   if (p[])
-  //   dp[j][0] = false;
-  // }
 
   for (let i = 0; i < sLen; i++) {
     for (let j = 0; j < pLen; j++) {
-      if (s[i] === p[j] || p[j] === ".") {
-        dp[i][j] === dp[i - 1][j - 1];
+      if (p.charAt(j) == ".") {
+        dp[i + 1][j + 1] = dp[i][j];
       }
-      if (p[j] === "*") {
-        if (p[j - 1] !== s[i]) {
-          dp[i][j] = dp[i][j - 2];
-        } else if (p[j - 1] === s[i] || p[j - 1] === ".") {
-          dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+      if (p.charAt(j) == s.charAt(i)) {
+        dp[i + 1][j + 1] = dp[i][j];
+      }
+      if (p.charAt(j) == "*") {
+        if (j >= 1 && p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != ".") {
+          dp[i + 1][j + 1] = dp[i + 1][j - 1];
+        } else {
+          if (j >= 1) {
+            dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+          }
         }
       }
     }
   }
-  console.log(dp);
-  return dp[s.length - 1][p.length - 1];
+  return dp[s.length][p.length];
 };
 
 export default isMatch;
--- test/top_interview/10_Regular_Expression_Matching/index.test.js
+++ test/top_interview/10_Regular_Expression_Matching/index.test.js
@@ -1,5 +1,12 @@
 import isMatch from "../../../src/top_interview/10_Regular_Expression_Matching/index";
 
-test("1", () => {
-  expect(isMatch("ab", ".*")).toBe(true);
+// test("1", () => {
+//   expect(isMatch("ab", ".*")).toBe(true);
+// });
+
+test("2", () => {
+  expect(isMatch("aa", "a*")).toBe(true);
+});
+test("3", () => {
+  expect(isMatch("", "c*c*")).toBe(true);
 });
