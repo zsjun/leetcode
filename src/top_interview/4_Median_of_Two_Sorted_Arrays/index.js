1aedf20252662d0909c050c968a08dced71efefc
zsj <zoushijun@threatbook.cn>
2020-10-21 14:58:15 +0800

    add top_interview/4

--- src/top_interview/4_Median_of_Two_Sorted_Arrays/index.js
+++ src/top_interview/4_Median_of_Two_Sorted_Arrays/index.js
@@ -1,59 +1,58 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
+ * @param {number[]} A
+ * @param {number[]} B
  * @return {number}
  */
+export default (A, B) => {
+  let m = A.length;
+  let n = B.length;
 
-export default (nums1, nums2) => {
-  let m = nums1.length;
-  let n = nums2.length;
-
-  // 保持m是最小的
+  // 保持m是最小的, 交换两个数组
   if (m > n) {
-    const temp = nums1;
     const temp1 = m;
-    nums1 = nums2;
     m = n;
-    nums2 = temp;
     n = temp1;
+
+    const temp = A;
+    A = B;
+    B = temp;
   }
-  if (n === 0) {
-    return false;
-  }
+  // if (n === 0) {
+  //   return false;
+  // }
   let imin = 0;
   let imax = m;
   let halfLen = Math.floor((m + n + 1) / 2);
-  console.log(2332, halfLen);
+  // console.log(2332, halfLen);
   let max_of_left = 0;
   let min_of_right = 0;
   while (imin <= imax) {
     let i = Math.floor((imin + imax) / 2);
     let j = halfLen - i;
 
-    if (i < m && nums2[j] > nums1[i]) {
+    if (i < m && j >= 1 && B[j - 1] > A[i]) {
       imin = i + 1;
-    } else if (i > 0 && nums1[i - 1] > nums2[j]) {
+    } else if (i >= 1 && A[i - 1] > B[j]) {
       imax = i - 1;
     } else {
-      if (i === 0) {
-        max_of_left = nums2[j - 1];
-      } else if (j === 0) {
-        max_of_left = nums1[i - 1];
+      if (i === 0 && j >= 1) {
+        max_of_left = B[j - 1];
+      } else if (j === 0 && i >= 1) {
+        max_of_left = A[i - 1];
       } else {
-        max_of_left = Math.max(nums1[i - 1], nums2[j - 1]);
+        max_of_left = Math.max(A[i - 1], B[j - 1]);
       }
-      console.log(max_of_left, min_of_right);
       if ((m + n) % 2 !== 0) {
         return max_of_left;
       }
-      if (i == m) {
-        min_of_right = nums2[j];
-      } else if (j === n) {
-        min_of_right = nums1[i];
+
+      if (i === m && j < n) {
+        min_of_right = B[j];
+      } else if (j === n && i < m) {
+        min_of_right = A[i];
       } else {
         min_of_right = Math.min(A[i], B[j]);
       }
-
       return (max_of_left + min_of_right) / 2;
     }
   }
--- test/top_interview/4_Median_of_Two_Sorted_Arrays/index.test.js
+++ test/top_interview/4_Median_of_Two_Sorted_Arrays/index.test.js
@@ -3,3 +3,7 @@ import findMedianSortedArrays from "../../../src/top_interview/4_Median_of_Two_S
 test("1", () => {
   expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
 });
+
+test("2", () => {
+  expect(findMedianSortedArrays([1, 2], [3, 4])).toBe(2.5);
+});
