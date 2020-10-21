57e5e88da5ba705611d2d42858c390ee94641198
zsj <zoushijun@threatbook.cn>
2020-10-21 12:53:16 +0800

    add top_interview/4

---
+++ test/top_interview/4_Median_of_Two_Sorted_Arrays/index.test.js
@@ -0,0 +1,5 @@
+import findMedianSortedArrays from "../../../src/top_interview/4_Median_of_Two_Sorted_Arrays/index";
+
+test("1", () => {
+  expect(findMedianSortedArrays([1, 3], [2])).toBe(2);
+});
