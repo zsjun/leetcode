de9e420aef4b2ef4d1ae9c3849d1f1ce04cf7dc6
zsj <zoushijun@threatbook.cn>
2020-11-02 12:10:53 +0800

    add top/8

---
+++ src/top_interview/8_String_to_Integer/index1.js
@@ -0,0 +1,29 @@
+export default (str) => {
+  str = str.trim();
+  if (!str) return 0;
+  let sign = 1;
+  let i = 0;
+  let answer = 0;
+
+  if (str[i] == "+") {
+    sign = 1;
+    i++;
+  } else if (str[i] == "-") {
+    sign = -1;
+    i++;
+  }
+
+  for (; i < str.length; i++) {
+    const temp = str.charCodeAt(i) - 48;
+    // 如果少于0或者9就是在不是数字范围之内
+    if (temp > 9 || temp < 0) break;
+    // 判断最大值和最小值
+    if (answer > 2147483647 / 10 || answer > (2147483647 - temp) / 10) {
+      return sign == 1 ? 2147483647 : -2147483648;
+    } else {
+      answer = answer * 10 + temp;
+    }
+  }
+
+  return answer * sign;
+};
