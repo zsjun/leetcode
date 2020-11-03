57da57efba88c3bcaf1cb1e6928339cd386d1e3d
zsj <zoushijun@threatbook.cn>
2020-11-03 10:39:14 +0800

    add topview/54

--- src/top_interview/54_Spiral_Matrix/index.js
+++ src/top_interview/54_Spiral_Matrix/index.js
@@ -2,56 +2,42 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-const getRes = (matrix, rowStart, rowEnd, colStart, colEnd, res) => {
-  // console.log(2, 1, 2, 2);
-  if (rowStart >= rowEnd || colStart >= colEnd) return;
-  // 上边
-  if (colEnd - colStart > 1) {
-    for (let i = colStart; i < colEnd; i++) {
-      if (matrix[rowStart][i] || matrix[rowStart][i] === 0) {
-        res.push(matrix[rowStart][i]);
-      }
-    }
-  }
-
-  // 右边
-  if (rowEnd - rowStart > 1) {
-    for (let i = rowStart + 1; i < rowEnd; i++) {
-      if (matrix[i][colEnd - 1] || matrix[i][colEnd - 1] === 0) {
-        res.push(matrix[i][colEnd - 1]);
-      }
-    }
-  }
-
-  if (rowEnd - rowStart > 1) {
-    for (let i = colEnd - 2; i >= colStart; i--) {
-      if (matrix[rowEnd - 1][i] || matrix[rowEnd - 1][i] === 0) {
-        res.push(matrix[rowEnd - 1][i]);
-      }
-    }
-  }
-  // 左边
-  if (rowEnd - rowStart > 1) {
-    console.log(res);
-    for (let i = rowEnd - 2; i >= rowStart + 1; i--) {
-      if (matrix[i][colStart + 1] || matrix[i][colStart + 1] === 0) {
-        res.push(matrix[i][colStart]);
-      }
-    }
-    console.log(rowStart, rowEnd, colStart, colEnd);
-    console.log(res);
-  }
-
-  getRes(matrix, rowStart + 1, rowEnd - 1, colStart + 1, colEnd - 1, res);
-};
-const getSprial = (matrix) => {
-  let res = [];
+const spiralOrder = (matrix) => {
   if (matrix.length === 0) {
     return [];
   }
-  getRes(matrix, 0, matrix.length, 0, matrix[0].length, res);
+  let res = [];
+  let rowStart = 0,
+    rowEnd = matrix.length - 1,
+    colStart = 0,
+    colEnd = matrix[0].length - 1;
 
+  while (rowStart <= rowEnd && colStart <= colEnd) {
+    for (let i = colStart; i <= colEnd; i++) {
+      res.push(matrix[rowStart][i]);
+    }
+    rowStart++;
+
+    for (let j = rowStart; j <= rowEnd; j++) {
+      res.push(matrix[j][colEnd]);
+    }
+    colEnd--;
+
+    if (rowStart <= rowEnd) {
+      for (let i = colEnd; i >= colStart; i--) {
+        res.push(matrix[rowEnd][i]);
+      }
+    }
+    rowEnd--;
+
+    if (colStart <= colEnd) {
+      for (let j = rowEnd; j >= rowStart; j--) {
+        res.push(matrix[j][colStart]);
+      }
+    }
+    colStart++;
+  }
   return res;
 };
 
-export default getSprial;
+export default spiralOrder;
