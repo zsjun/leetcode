11b5ec1bd4436cb67023ee2c66d60862b531bf2f
zsj <zoushijun@threatbook.cn>
2020-11-04 11:08:30 +0800

    add top_interview/116

--- src/top_interview/116_Populating_Next_Right_Pointers_in_Each_Node/index.js
+++ src/top_interview/116_Populating_Next_Right_Pointers_in_Each_Node/index.js
@@ -17,33 +17,36 @@
 // }
 const connect = (root) => {
 
-  // if (root == NULL) return;
-  // let pre = root;
-  // let cur = NULL
-  // while(pre.left) {
-  //     cur = pre;
-  //     while(cur) {
-  //         cur.left.next = cur.right;
-  //         if(cur.next) cur.right.next = cur.next.left;
-  //         cur = cur.next;
-  //     }
-  //     pre = pre.left;
-  // }
-
-  if(!root || !root.left) return root
-  if(root && root.left) {
-    root.left.next = root.right
-
+  if (!root || !root.left) return root;
+  let pre = root;
+  let cur = null;
+  while(pre.left) {
+      cur = pre;
+      while(cur) {
+          cur.left.next = cur.right;
+          if(cur.next) {
+            cur.right.next = cur.next.left;
+          }
+          cur = cur.next;
+      }
+      pre = pre.left;
   }
-  if(root && root.right && root.next) {
-    if(root.next) {
-      root.right.next = root.next.left;
-    }
-
-  }
-  connect(root.left)
-  connect(root.right)
   return root;
+
+  // if(!root || !root.left) return root
+  // if(root && root.left) {
+  //   root.left.next = root.right
+
+  // }
+  // if(root && root.right && root.next) {
+  //   if(root.next) {
+  //     root.right.next = root.next.left;
+  //   }
+
+  // }
+  // connect(root.left)
+  // connect(root.right)
+  // return root;
 
 
 };
