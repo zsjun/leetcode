2d6ece5146335e4da81cdff0486a22464e539ce6
zsj <zoushijun@threatbook.cn>
2020-11-10 16:04:24 +0800

    change createDir

--- createDir.js
+++ createDir.js
@@ -5,5 +5,5 @@ const nameArr = process.argv.slice(2);
 
 const fileName = nameArr.join("_").replace(".", "");
 
-mkdirp(`./test/top_interview/${fileName}`).then(() => console.log(`made test dir ${fileName}`));
-mkdirp(`./src/top_interview/${fileName}`).then(() => console.log(`made src dir  ${fileName}`));
+mkdirp(`./test/bfs/${fileName}`).then(() => console.log(`made test dir ${fileName}`));
+mkdirp(`./src/bfs/${fileName}`).then(() => console.log(`made src dir  ${fileName}`));
