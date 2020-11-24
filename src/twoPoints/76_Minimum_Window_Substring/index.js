6cbd84b47a4886f645231bb2fafb2cb5758953e2
zsj <zoushijun@threatbook.cn>
2020-11-24 15:15:21 +0800

    add twoPoints/76

--- src/twoPoints/76_Minimum_Window_Substring/index.js
+++ src/twoPoints/76_Minimum_Window_Substring/index.js
@@ -4,49 +4,43 @@
  * @return {string}
  */
 export default (s, t) => {
-  const flag = new Map();
-  const chars = new Map();
+  const tMap = new Map();
   for (let i = 0; i < t.length; i++) {
-    flag.set(t[i], true);
-    if (!chars.has(t[i])) {
-      chars.set(t[i], 1);
+    if (!tMap.has(t[i])) {
+      tMap.set(t[i], 1);
     } else {
-      let count = chars.get(t[i]) + 1;
-      chars.set(t[i], count);
+      let count = tMap.get(t[i]) + 1;
+      tMap.set(t[i], count);
     }
   }
 
   let cnt = 0,
-    l = 0,
-    min_l = 0,
-    min_size = s.length + 1;
+    slow = 0,
+    resStart = 0,
+    resLen = s.length + 1;
 
-  for (let r = 0; r < s.length; r++) {
-    // console.log(flag.has(s[r]), s[r]);
-    if (flag.has(s[r])) {
-      let count = chars.get(s[r]) - 1;
-      chars.set(s[r], count);
+  for (let fast = 0; fast < s.length; fast++) {
+    if (tMap.has(s[fast])) {
+      let count = tMap.get(s[fast]) - 1;
+      tMap.set(s[fast], count);
       if (count >= 0) {
         cnt++;
       }
       // 若目前滑动窗口已包含T中全部字符，
       // 则尝试将l右移，在不影响结果的情况下获得最短子字符串
-      // console.log(cnt, s[l], flag.has(s[l]), chars.get(s[l]) + 1);
       while (cnt === t.length) {
-        if (r - l + 1 < min_size) {
-          min_l = l;
-          min_size = r - l + 1;
+        if (fast - slow + 1 < resLen) {
+          resStart = slow;
+          resLen = fast - slow + 1;
         }
-        let count = chars.get(s[l]) + 1;
-        chars.set(s[l], count);
-        if (flag.has(s[l]) && count > 0) {
+        let count = tMap.get(s[slow]) + 1;
+        tMap.set(s[slow], count);
+        if (tMap.has(s[slow]) && count > 0) {
           cnt--;
         }
-        l++;
+        slow++;
       }
-      // console.log(min_l, min_size);
     }
   }
-  console.log(min_l, min_size);
-  return min_size > s.length ? "" : s.substr(min_l, min_size);
+  return resLen > s.length ? "" : s.substr(resStart, resLen);
 };
