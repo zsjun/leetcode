237a0e063ea8b559d651db713af36d3083be80d8
zsj <zoushijun@threatbook.cn>
2020-12-01 14:47:41 +0800

    change greed/55

--- src/greed/55_Jump_Game/index3.js
+++ src/greed/55_Jump_Game/index3.js
@@ -5,10 +5,11 @@
 // 76 ms 40 MB
 // dp[i] 表示到达i剩余步数
 // 到达dp[i-1] i-1 剩余的步数，
-// 题目首先要理解清楚
+// 题目首先要理解清楚，妈的一直陷入了死循环，一直以为跳到这步，然后需要加上现在剩下的，
 export default (nums) => {
   const len = nums.length;
   let dp = new Array(len).fill(0);
+
   for (let i = 1; i < len; i++) {
     dp[i] = Math.max(dp[i - 1], nums[i - 1]) - 1;
     if (dp[i] < 0) return false;
