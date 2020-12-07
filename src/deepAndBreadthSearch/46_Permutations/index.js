fb53a17e596075f6de60823b1b43fcfadb47f5be
zsj <zoushijun@threatbook.cn>
2020-12-07 12:00:00 +0800

    add src/deepAndBreadthSearch

--- src/deepAndBreadthSearch/46_Permutations/index.js
+++ src/deepAndBreadthSearch/46_Permutations/index.js
@@ -8,19 +8,28 @@ const swap = (nums, i, j) => {
   nums[j] = temp;
 };
 const backtracking = (nums, index, res) => {
-  res.push([...nums]);
-  for (let i = index + 1; i < nums.length; i++) {
-    swap(nums, index, i);
+  if (index === nums.length - 1) {
+    res.push([...nums]);
+  }
+  for (let i = index; i < nums.length; i++) {
+    if (i !== index) {
+      swap(nums, i, index);
+    }
+
     backtracking([...nums], index + 1, res);
-    swap(nums, i, index);
+    if (i !== index) {
+      swap(nums, index, i);
+    }
   }
 };
+// Runtime: 92 ms, faster than 85.19% of JavaScript online submissions for Permutations.
+// Memory Usage: 41.3 MB, less than 79.69% of JavaScript online submissions for Permutations.
 export default (nums) => {
   if (nums.length === 1) {
     return [[nums[0]]];
   }
   let res = [];
   backtracking(nums, 0, res);
-  console.log(res);
+  console.log(11, res);
   return res;
 };
