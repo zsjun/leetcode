9629a48bc3bd856d1f49850a7ed5dcf904390803
zsj <zousjun@126.com>
2022-02-16 18:28:32 +0800

    82

---
+++ src/everyday/82_Remove_Duplicates_from_Sorted_List_II/index.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (!head) return null;
+  let newHead = null;
+  while (head) {
+    if (head && head.next && head.val === head.next.val) {
+      while (head && head.next && head.val === head.next.val) {
+        head = head.next;
+      }
+      head = head.next;
+    } else {
+      newHead = head;
+      break;
+    }
+  }
+
+  let pre = newHead;
+  let cur = pre ? pre.next : null;
+  while (cur && cur.next) {
+    if (cur && cur.next && cur.val === cur.next.val) {
+      while (cur && cur.next && cur.val === cur.next.val) {
+        cur = cur.next;
+      }
+      pre.next = cur.next;
+      cur = cur.next;
+    } else {
+      pre = cur;
+      cur = cur.next;
+    }
+  }
+  return newHead;
+};
